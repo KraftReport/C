bb1399bebeba060f7a465978eaf3056d09c73964
Myo Set Paing <138693417+KraftReport@users.noreply.github.com>
2024-12-22 20:51:07 +0630

    first day

--- FirstCProgram/main.c
+++ FirstCProgram/main.c
@@ -4,5 +4,26 @@
 int main()
 {
     printf("Hello world!\n");
-    return 0;
+
+    int fahrenheit;
+    int celsius;
+    int lower;
+    int upper;
+    int step;
+
+    lower = 0;
+    upper = 300;
+    step = 20;
+
+    fahrenheit = lower;
+
+    while(fahrenheit <= upper){
+        celsius = 5 * (fahrenheit-32) / 9;
+        printf("%d\t%d\n",fahrenheit,celsius);
+        fahrenheit = fahrenheit + step;
+    }
+
+    system("pause"); // For Windows
+
+
 }
